521b80f7d05c655e0f4c1038b68debef8212c1d2
Jeremy <SourceTestFTW@gmail.com>
2015-09-15 02:06:57 -0400

    Removed the copyrighted code from index4.js

--- index4.js
+++ index4.js
@@ -1,9 +1,7 @@
 //	Customization
-
 var appPort = 3000;
 
-// Librairies
-
+//Libs
 var express = require('express'), app = express();
 var http = require('http')
   , server = http.createServer(app)
@@ -16,7 +14,6 @@ var jade = require('jade');
 var pseudoArray = ['admin']; //block the admin username (you can disable it)
 
 // Views Options
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
@@ -24,7 +21,6 @@ app.set("view options", { layout: false });
 app.use(express.static(__dirname + '/public'));
 
 // Render and send the main page
-
 app.get('/', function(req, res){
   res.render('index.jade');
 });
@@ -33,46 +29,13 @@ server.listen(appPort);
 console.log("Server listening on port " + appPort);
 
 // Handle the socket.io connections
-
-var users = 0; //count the users
-
-io.sockets.on('connection', function (socket) { // First connection
-	//users += 1; // Add 1 to the count
-	//reloadUsers(); // Send the count to all the users
+io.sockets.on('connection', function (socket) { // init connection
 	socket.on('message', function (data) { // Broadcast the message to all
-		/*if(pseudoSet(socket))
-		{
-			var transmit = {date : new Date().toISOString(), pseudo : socket.nickname, message : data};
-			socket.broadcast.emit('message', transmit);
-			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
-		}*/
-	});
-	socket.on('setPseudo', function (data) { // Assign a name to the user
-		if (pseudoArray.indexOf(data) == -1) // Test if the name is already taken
-		{
-			pseudoArray.push(data);
-			socket.nickname = data;
-			socket.emit('pseudoStatus', 'ok');
-			console.log("user " + data + " connected");
-		}
-		else
-		{
-			socket.emit('pseudoStatus', 'error') // Send the error
-		}
-	});
-	socket.on('disconnect', function () { // Disconnection of the client
-		users -= 1;
-		reloadUsers();
-		if (pseudoSet(socket))
-		{
-			console.log("disconnect...");
-			var pseudo;
-			pseudo = socket.nickname;
-			var index = pseudoArray.indexOf(pseudo);
-			pseudo.slice(index - 1, 1);
-		}
-	});
 
+	});
+	socket.on('disconnect', function () {
+
+	});
 	socket.on("objectTable", function (objectsFromTable) {
 
         // we received a tweet from the browser
@@ -98,18 +61,4 @@ io.sockets.on('connection', function (socket) { // First connection
         socket.emit("requestSubmitted", items);
         });
     });
-
-
-
-
 });
-
-function reloadUsers() { // Send the count of the users to all
-	io.sockets.emit('nbUsers', {"nb": users});
-}
-function pseudoSet(socket) { // Test if the user has a name
-	var test;
-	if (socket.nickname == null ) test = false;
-	else test = true;
-	return test;
-}
